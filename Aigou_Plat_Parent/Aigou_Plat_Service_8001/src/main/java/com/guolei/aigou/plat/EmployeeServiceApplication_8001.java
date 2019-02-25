97b19fb8e5471e3484b78d79e0e8371137bfc72a
Taurus <313083686@qq.com>
2019-02-25 21:21:29 +0800

    上传到master

--- Aigou_Plat_Parent/Aigou_Plat_Service_8001/src/main/java/com/guolei/aigou/plat/EmployeeServiceApplication_8001.java
+++ Aigou_Plat_Parent/Aigou_Plat_Service_8001/src/main/java/com/guolei/aigou/plat/EmployeeServiceApplication_8001.java
@@ -9,6 +9,11 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 public class EmployeeServiceApplication_8001 {
 
     public static void main(String[] args) {
+//        合并主干----》在主干
+        /**
+         * 文档注释
+         */
         SpringApplication.run(EmployeeServiceApplication_8001.class, args);
+        System.out.println("主干");
     }
 }
